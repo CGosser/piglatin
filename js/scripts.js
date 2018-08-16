2b36c328d3451e35b77868312f1c24c1a87ffc3a
Chris Gosser and Dustin Starke <chris@>
2018-08-16 10:27:25 -0700

    include new functions to append and sort words

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,58 @@
 // business logic
 function isVowel(letter){
-    if (letter === "a"|| letter === "e"|| letter === "i"|| letter === "o"|| letter === "u"|| letter === "y") {
-      return true
-    } else {
-      return false
-    }
+  if (letter === "a"|| letter === "e"|| letter === "i"|| letter === "o"|| letter === "u"|| letter === "y") {
+    return true
+  } else {
+    return false
   }
-function isWord(userText) { //splits string into array seperated by the spaces
-  return userText.split(" ")
+}
+var userInputs = []
+function inputArray(userText) { //splits string into array seperated by the spaces
+  userInputs = userText.split(" ")
 
 }
-var latinize = "" // this is the string we will be manipulate, then feed into a new array which is not defined yet
-function firstword(firstarray) {
-  latinize = firstarray.shift(0)
+var latinizedWord = "" // this is the string we will be manipulate, then feed into a new array which is not defined yet
+function userWord() {
+  return .shift(0)
 }
 var i = 0
 function firstVowel(workWith) { //checks for position of first vowel
   while(isVowel(workWith[i]) != true){
+    if(workWith[i] === "q" && workWith[i+1] === "u"){
+      i++
+      // console.log(i);
+    }
     i++
+    // console.log(i)
   }
-  return i-1
+  return i
+}
+var finalOutput = []
+function chopFront(takenWord) { //take user input and slice to the index
+  var sliceHere = firstVowel(takenWord)
+  var hackedBegin = (takenWord.slice(0, sliceHere))
+  return takenWord.slice(sliceHere, -0) + hackedBegin + "ay"
+  console.log(hackedBegin);
 }
 
-
+function appendWay(word) {
+  word + "way"
+}
+function appendAy(word) {
+  word + "ay"
+}
+function sortingHat(word) {
+  if (word.length === 1) {
+    appendAy(word)
+  } else if (firstVowel(word) === 0) {
+    appendWay(word)
+  } else if (firstVowel(word) > 0) {
+    chopFront(word)
+  }
+}
+function finalLatin() {
+  finalOutput.push(sortingHat())
+}
 // (/\W/g.test())
 
 
@@ -30,6 +60,8 @@ function firstVowel(workWith) { //checks for position of first vowel
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
+
+
     // var year = parseInt($("input#year").val());
     // var result = leapYear(year);
     //
@@ -45,5 +77,6 @@ $(document).ready(function() {
 
     // console.log(isWord($("#input").val()));
   });
+  console.log(firstVowel("squeal"));
   // console.log(isVowel("k"));;
 });
